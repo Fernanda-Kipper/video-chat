9fb318548d6dd546a9c84a5c3b490d3a56b89a33
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-10-24 22:46:00 -0300

    feat: setup project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Video-Chat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
