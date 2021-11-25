7d1987b183e3240495a72ef08b650c70c2d5c4fc
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-11-25 16:38:35 -0300

    feat(call-system): create store context

--- client/src/contexts/store-context.tsx
+++ client/src/contexts/store-context.tsx
@@ -1,18 +1,16 @@
-// import { createContext, ReactNode } from "react";
-// import { rootStore, RootStoreType } from "../stores/root.store";
+import { createContext, ReactNode } from "react";
+import { rootStore, RootStoreType } from "../stores/root.store";
 
-// interface Props {
-//   children: ReactNode
-// }
+interface Props {
+  children: ReactNode
+}
 
-// export const storeContext = createContext<RootStoreType>({})
+export const storeContext = createContext({} as RootStoreType)
 
-// export function StoreProvider({ children }: Props){
-//   return(
-//     <storeContext.Provider value={rootStore}>
-//       {children}
-//     </storeContext.Provider>
-//   )
-// }
-
-export const store = () => {}
+export function StoreProvider({ children }: Props){
+  return(
+    <storeContext.Provider value={rootStore}>
+      {children}
+    </storeContext.Provider>
+  )
+}
