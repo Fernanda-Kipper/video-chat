581a8f343dbeb1b7d8f70c8d5167f3d82387253a
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-11-26 17:20:39 -0300

    feat(master): added loading animation when calling

--- client/src/components/loading-overlay.tsx
+++ client/src/components/loading-overlay.tsx
@@ -1,19 +1,91 @@
-import { Modal, ModalOverlay, ModalContent, Text } from "@chakra-ui/react"
+import { Modal, ModalOverlay, ModalContent, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion'
 
-interface Props {
-  loading: boolean
+const loadingContainer = {
+  width: "50%",
+  height: "2rem",
+  display: "flex",
+  justifyContent: "space-around"
+};
+
+const loadingCircle = {
+  display: "block",
+  width: "1.5rem",
+  height: "1.5em",
+  borderRadius: "0.75rem",
+  backgroundColor: "#6b46c1"
+};
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
 }
 
-export function LoadingOverlay(props: Props){
-  if (!props.loading) return <></>
+const loadingCircleVariants = {
+  start: {
+    y: "0%",
+  },
+  end: {
+    y: "100%",
+  },
+}
 
-  return(
+const loadingCircleTransition = {
+  duration: 0.5,
+  yoyo: Infinity,
+  ease: "easeInOut",
+}
+
+interface Props {
+  loading: boolean;
+}
+
+export function LoadingOverlay(props: Props) {
+  if (!props.loading) return <></>;
+
+  return (
     <Modal isOpen={props.loading} isCentered onClose={() => {}}>
       <ModalOverlay>
-        <ModalContent p="16" backgroundColor="gray.800">
-          <Text color="white" fontWeight="bold">Calling ...</Text>
+        <ModalContent
+          p="16"
+          backgroundColor="gray.800"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text color="purple.600" fontWeight="bold" fontSize="2rem">Calling</Text>
+          <motion.div
+            style={loadingContainer}
+            variants={loadingContainerVariants}
+            initial="start"
+            animate="end"
+          >
+            <motion.span
+              style={loadingCircle}
+              variants={loadingCircleVariants}
+              transition={loadingCircleTransition}
+            />
+            <motion.span
+              style={loadingCircle}
+              variants={loadingCircleVariants}
+              transition={loadingCircleTransition}
+            />
+            <motion.span
+              style={loadingCircle}
+              variants={loadingCircleVariants}
+              transition={loadingCircleTransition}
+            />
+          </motion.div>
         </ModalContent>
       </ModalOverlay>
     </Modal>
-  )
+  );
 }
